ddb075ecc4541cd53fe4e3e4fe58c4b98c6013a3
scroft001 <scroft001@gmail.com>
2015-03-12 00:59:16 -0700

    The first homework
    Displays 2 print lines

---
+++ First Homework/FirstHomework.java
@@ -0,0 +1,8 @@
+public class FirstHomework
+{
+	public static void main( String [] args )
+	{
+		System.out.println("Evacuate in our moment of triumph?");
+		System.out.println("I think you overestimate their chances");
+	}
+}
