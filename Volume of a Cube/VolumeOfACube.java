29a41275f33d042f4556b0d7aca799c70c07d8a7
scroft001 <scroft001@gmail.com>
2015-03-12 10:52:04 -0700

    The volume of a cube
    Finds the volume of a cube

---
+++ Volume of a Cube/VolumeOfACube.java
@@ -0,0 +1,26 @@
+/* Steven Croft homework 3 volume of a cube */
+
+public class VolumeOfACube
+{
+	public static void main (String [] args)
+	{
+		//variables
+		double length;
+		double height;
+		double width;
+		double volume;
+		
+		//assign variables
+		length = 10;
+		height = 10;
+		width = 10;
+		
+		//assign volume equation
+		volume = length * height * width;
+		
+		//output
+		System.out.println("The volume of the cube is " + volume);
+		
+	}
+
+}
